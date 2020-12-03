a4989b497c79bda3564e805662eed4d8641ad807
Yeremeev <tonaros.kill@gmail.com>
2020-12-03 18:13:36 +0200

    added a class that stores movement binds

---
+++ Assets/Scripts/KeyboardBindings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+public class KeyboardBindings
+{
+    public KeyCode KeyRight {get; set;} = KeyCode.D;
+    public KeyCode KeyLeft {get; set;} = KeyCode.A;
+    public KeyCode KeyForward {get; set;} = KeyCode.W;
+    public KeyCode KeyBack {get; set;} = KeyCode.S;
+    public KeyCode KeyJump {get; set;} = KeyCode.Space;
+}
