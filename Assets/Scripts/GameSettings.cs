129cf8e301f4fdb3084da423fa503a0fdbb197af
Yeremeev <tonaros.kill@gmail.com>
2020-12-03 19:20:07 +0200

    Game settings container class and enums for some settings added

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameSettings
+    {
+        /// <summary>
+        /// game screen resolution
+        /// </summary>
+        public Vector2 ScreenResolution { get; set; }
+        /// <summary>
+        /// should the game be in full screen mode
+        /// </summary>
+        public bool IsFullscreen { get; set; }
+        public Quality ShadowQuality { get; set; }
+        /// <summary>
+        /// distance to draw shadows
+        /// </summary>
+        public Distance ShadowDistance { get; set; }
+        public Quality GraphicQuality { get; set; }
+        /// <summary>
+        /// game background music
+        /// </summary>
+        public int MusicVolume { get; set; }
+        /// <summary>
+        /// ambient sound volume
+        /// </summary>
+        public int SfxVolume { get; set; }
+        /// <summary>
+        /// game interface language
+        /// </summary>
+        public Languages Language { get; set; }
+    }
+}
---
+++ Assets/Scripts/Quality.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts
+{
+    public enum Quality
+    {
+        Low,
+        Medium,
+        High
+    }
+}
