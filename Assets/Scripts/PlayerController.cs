73eb4752b241f8f3d17a5ee6c1cc1f13c52df470
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-12-03 18:19:29 +0200

    Implemented basic player movement implementation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 6.0f;
+    public float Gravity = -9.8f;
+
+    private readonly InputSystem _inputSystem = new InputSystem();
+    private CharacterController _characterController;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        var deltaX = _inputSystem.GetHorizontalMovementValue() * Speed ;
+        var deltaZ = _inputSystem.GetVerticalMovementValue() * Speed;
+        var movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, Speed);
+        movement.y = Gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _characterController.Move(movement);
+    }
+}
