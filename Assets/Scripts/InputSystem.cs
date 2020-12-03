25a679de738d7f1bba9fb905d24bf22caaf69bbb
Yeremeev <tonaros.kill@gmail.com>
2020-12-03 18:11:24 +0200

    Update get movement implementation

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -1,6 +1,34 @@
 ﻿using UnityEngine;
 public class InputSystem
 {
-    public float GetHorizontalAxis() { return Input.GetAxisRaw("Horizontal");}
-    public float GetVerticalAxis() { return Input.GetAxisRaw("Vertical");}
+    private KeyboardBindings _bindings;
+    public InputSystem()
+    {
+        _bindings = new KeyboardBindings();
+    }
+    public float GetHorizontalMovementValue()
+    {
+        if(Input.GetKey(_bindings.KeyRight))
+        {
+            return 1f;
+        }
+        if (Input.GetKey(_bindings.KeyLeft))
+        {
+            return -1f;
+        }
+        return 0f;
+    }
+
+    public float GetVerticalMovementValue()
+    {
+        if (Input.GetKey(_bindings.KeyForward))
+        {
+            return 1f;
+        }
+        if (Input.GetKey(_bindings.KeyBack))
+        {
+            return -1f;
+        }
+        return 0f;
+    }
 }
