fd594cda3a469a16d77af56fa8f80674d49f4a85
Yeremeev <tonaros.kill@gmail.com>
2020-12-04 17:55:02 +0200

    update MouseLook - added InputSystem reference and Cursor state flag

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -10,11 +10,15 @@ namespace Assets.Scripts
         public float SensitivityVert = 9.0f;
         public float MinimumVert = -45.0f;
         public float MaximumVert = 45.0f;
+        public bool ShowCursor = false;
+        
         private float _rotationX = 0;
-    
+        private InputSystem _inputSystem; 
+        
         void Start()
         {
-            Cursor.visible = false;
+            Cursor.visible = ShowCursor;
+            _inputSystem = new InputSystem();
             Rigidbody body = GetComponent<Rigidbody>();
             if (body != null)
                 body.freezeRotation = true;
@@ -24,20 +28,20 @@ namespace Assets.Scripts
         {
             if (Axes == RotationAxes.MouseX)
             {
-                transform.Rotate(0, Input.GetAxis("Mouse X") * SensitivityHor, 0);
+                transform.Rotate(0,  _inputSystem.GetMouseXAxis() * SensitivityHor, 0);
             }
             else if (Axes == RotationAxes.MouseY)
             {
-                _rotationX -= Input.GetAxis("Mouse Y") * SensitivityVert;
+                _rotationX -= _inputSystem.GetMouseYAxis() * SensitivityVert;
                 _rotationX = Mathf.Clamp(_rotationX, MinimumVert, MaximumVert);
                 float rotationY = transform.localEulerAngles.y;
                 transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
             }
             else
             {
-                _rotationX -= Input.GetAxis("Mouse Y") * SensitivityVert;
+                _rotationX -= _inputSystem.GetMouseYAxis() * SensitivityVert;
                 _rotationX = Mathf.Clamp(_rotationX, MinimumVert, MaximumVert);
-                float delta = Input.GetAxis("Mouse X") * SensitivityHor;
+                float delta = _inputSystem.GetMouseXAxis() * SensitivityHor;
                 float rotationY = transform.localEulerAngles.y + delta;
                 transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
             }
