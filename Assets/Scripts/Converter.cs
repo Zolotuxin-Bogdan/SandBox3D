24d8fd919537ebf06a1065e42c8e1e46f672cf34
Yeremeev <tonaros.kill@gmail.com>
2020-12-10 13:41:55 +0200

    added converter method that convert index to enum value

---
+++ Assets/Scripts/Converter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Assets.Scripts
+{
+    public class Converter
+    {
+        /// <summary>
+        /// Get enum value by Id
+        /// </summary>
+        /// <typeparam name="T">Enum</typeparam>
+        /// <param name="id">enum value id</param>
+        /// <returns><see cref="string"/></returns>
+        public static string FromIdToString<T>(int id)
+        {
+            var names = Enum.GetNames(typeof(T));
+            return names[id];
+        }
+    }
+}
