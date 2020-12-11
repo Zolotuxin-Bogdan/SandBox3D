b3aa23e401d52edcba12de21dcf5bad80c3361d4
Yeremeev <tonaros.kill@gmail.com>
2020-12-11 19:58:24 +0200

    UI updated - addded Controls, TexturePacks and Languages screen's

--- Assets/Scripts/UI/ControlsController.cs
+++ Assets/Scripts/UI/ControlsController.cs
@@ -1,10 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.UI
 {
     public class ControlsController: MonoBehaviour
     {
+        public Button Done;
+
+        private void Start() {
+            Done.onClick.AddListener(Submit);
+        }
+
+        private void Submit()
+        {
+            action.Invoke();
+        }
+
         private UnityAction action;
         public void AddListener(UnityAction action)
         {
--- Assets/Scripts/UI/TexturePackController.cs
+++ Assets/Scripts/UI/TexturePackController.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.UI
 {
     public class TexturePackController: MonoBehaviour
     {
+        public Button Done;
+
+        private void Start() {
+            Done.onClick.AddListener(Submit);
+        }
+
+        private void Submit()
+        {
+            action.Invoke();
+        }
         private UnityAction action;
         public void AddListener(UnityAction action)
         {
