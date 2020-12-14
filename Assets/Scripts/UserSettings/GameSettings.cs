5b95d6f3779f65d18308096b3792a3e6380b96d9
Yeremeev <tonaros.kill@gmail.com>
2020-12-14 16:52:01 +0200

    updated settings naming

--- Assets/Scripts/UserSettings/GameSettings.cs
+++ Assets/Scripts/UserSettings/GameSettings.cs
@@ -1,4 +1,5 @@
-﻿using Assets.Scripts.Enums;
+﻿using System;
+using Assets.Scripts.Enums;
 using UnityEngine;
 
 namespace Assets.Scripts.UserSettings
@@ -8,14 +9,12 @@ namespace Assets.Scripts.UserSettings
         /// <summary>
         /// game screen resolution
         /// </summary>
+        [Obsolete("Property ScreenResolution is deprecated and will be updated soon.", false)]
         public Vector2 ScreenResolution { get; set; }
-        /// <summary>
-        /// should the game be in full screen mode
-        /// </summary>
-        public bool IsFullscreen { get; set; }
         /// <summary>
         /// distance to draw shadows
         /// </summary>
+        [Obsolete("Property ShadowDistance is deprecated and will be removed soon.", false)]
         public Distance ShadowDistance { get; set; }
         /// <summary>
         /// game background music
@@ -50,56 +49,76 @@ namespace Assets.Scripts.UserSettings
         /// </summary>
         public int FOV { get; set; }
         /// <summary>
-        /// game shadows quality
-        /// </summary>
-        public Quality Shadows { get; set; }
-        /// <summary>
-        /// game graphic quality
-        /// </summary>
-        public Quality Graphic { get; set; }
-        /// <summary>
         /// 
         /// </summary>
+        [Obsolete("Property Mipmap is deprecated and will be removed soon.", false)]
         public MipmapLevels Mipmap{ get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public Brightness Brightness { get; set; }
         /// <summary>
         /// max count of frames per second (121 - mean that frames count is unlimited)
         /// </summary>
+        [Obsolete("Property MaxFramerate is deprecated and will be removed soon.", false)]
         public int MaxFramerate { get; set; }
-        /// <summary>
-        /// world draw distance
-        /// </summary>
-        public int RenderDistance { get; set; }
         /// <summary>
         /// 
         /// </summary>
+        [Obsolete("Property BiomeBlend is deprecated and will be removed soon.", false)]
         public BiomeBlend BiomeBlend { get; set; }
         /// <summary>
         /// Enable/Disable shadows entity
         /// </summary>
+        [Obsolete("Property EntityShadows is deprecated and will be removed soon.", false)]
         public bool EntityShadows { get; set;}
+
+        // VIDEO(GRAPHIC) SETTINGS
+        
+        /// <summary>
+        /// game graphic quality
+        /// </summary>
+        public Quality graphic_quality { get; set; }
+        /// <summary>
+        /// should the game be in full screen mode
+        /// </summary>
+        public bool graphic_fullscreen { get; set; }
+        /// <summary>
+        /// brightness level of game objects
+        /// </summary>
+        public Brightness graphic_brightness { get; set; }
+        /// <summary>
+        /// world draw distance
+        /// </summary>
+        public int graphic_renderDistance { get; set; }
         /// <summary>
         /// Enable/Disable clouds
         /// </summary>
-        public bool Clouds { get; set; }
+        public bool graphic_clouds { get; set; }
         /// <summary>
         /// Enable/Disable view bobbing
         /// </summary>
-        public bool ViewBobbing { get; set; }
+        public bool graphic_viewBobbing { get; set; }
         /// <summary>
         /// Enable/Disable smooth lighting
         /// </summary>
-        public bool SmoothLighting { get; set; }
+        public bool graphic_smoothLighting { get; set; }
         /// <summary>
         /// Enable/Disable vertical sync
         /// </summary>
-        public bool UseVSync { get; set; }
+        public bool graphic_useVSync { get; set; }
         /// <summary>
         /// GUI size relative to original
         /// </summary>
-        public int GUIScale { get; set; }
+        public int graphic_guiScale { get; set; }
+
+        // CHAT AND MULTIPLAYER SETTINGS
+
+        public ChatDisplayStates chat_displayState { get; set; }
+        public bool chat_allowWebLinks { get; set; }
+        public bool chat_allowPromptOnLinks { get; set; }
+        public bool chat_allowColorsInChat { get; set; }
+        public bool multiplayer_allowCape { get; set; }
+        public float chat_scale { get; set; }
+        public float chat_opacity { get; set; }
+        public float chat_unfocusedHeight { get; set; }
+        public float chat_focusedHeight { get; set; }
+        public float chat_width { get; set; }
     }
 }
