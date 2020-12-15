80173eb53d87029b7f10b1bbb21193b58bc452cd
Yeremeev <tonaros.kill@gmail.com>
2020-12-15 22:00:31 +0200

    settings save/load logic added

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -5,14 +5,40 @@ namespace Assets.Scripts
 {
     public class SettingsManager: MonoBehaviour
     {
+        [Header("Save/Load Controls")]
+        public string relativePath = "\\config\\";
+        
+        string file;
         GameSettings settings;
-        private void Start() 
+        Storage settingsStorage;
+        private void Awake() 
         {
-            settings = new GameSettings();
+            file = "settings.json";
+            settingsStorage = new Storage();
+            TryLoadSettings();
         }
         public GameSettings GetSettings()
         {
             return settings;
+        }
+        public void SaveSettings()
+        {
+            settingsStorage.SaveData<GameSettings>(settings, Application.dataPath + relativePath + file);
+        }
+        public void TryLoadSettings()
+        {
+            try
+            {
+                settings = settingsStorage.LoadData<GameSettings>(Application.dataPath + relativePath + file);
+                print("settings loaded");
+            }
+            catch (System.Exception e)
+            {
+                settings = new GameSettings();
+                print("failed to load settings, create default instance");
+                Debug.LogError(e);
+                throw;
+            }
         }
     }
 }
