95419198a7e642fdc0758b1719be1e66f1e069fb
Yeremeev <tonaros.kill@gmail.com>
2020-12-17 21:58:57 +0200

    added FileDialog folder and controller for testing FileDialog

---
+++ Assets/FileDialog/DebugController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugController : MonoBehaviour
+{
+    DirectoryBrowser explorer;
+    bool drawExplorer;
+    public string path;
+    public Texture2D directoryIcon;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponentInChildren<Button>().onClick.AddListener(SomeButtonCallback);
+    }
+    
+    private void SomeButtonCallback()
+    {
+       drawExplorer = true;
+    }
+    
+    private void OnGUI()
+    {
+       if (explorer != null)
+           explorer.OnGUI();
+       else
+           OnGUIMain();
+    }
+    
+    private void OnGUIMain()
+    {
+        if (drawExplorer)
+        {
+			if (GUILayout.Button("...")) {
+				explorer = new DirectoryBrowser(
+                    "Choose folder...",
+                    new Rect(300, 100, 800, 600),
+                    FileSelectCallback
+                );
+				explorer.DirectoryIcon = directoryIcon;
+			}
+        }
+    }
+    
+    private void FileSelectCallback(string path)
+    {
+        explorer = null;
+        this.path = path;
+    }
+}
