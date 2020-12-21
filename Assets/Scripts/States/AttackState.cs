5932957cf4a944eedb9e3b3d9cf8589a108312ad
Yeremeev <tonaros.kill@gmail.com>
2020-12-21 21:52:17 +0200

    added attack and running states without implementation

---
+++ Assets/Scripts/States/AttackState.cs
@@ -0,0 +1,14 @@
+﻿using Assets.FSM;
+
+public class AttackState : State
+{
+    public override void Close()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Start()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/States/RunningState.cs
@@ -0,0 +1,14 @@
+﻿using Assets.FSM;
+
+public class RunningState : State
+{
+    public override void Close()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Start()
+    {
+        throw new System.NotImplementedException();
+    }
+}
