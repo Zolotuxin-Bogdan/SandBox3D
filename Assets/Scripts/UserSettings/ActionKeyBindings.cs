1faf25d0b2f9d532bb342ace6528f53f7c8c122a
Yeremeev <tonaros.kill@gmail.com>
2020-12-21 21:52:39 +0200

    added actions key bindings

---
+++ Assets/Scripts/UserSettings/ActionKeyBindings.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.UserSettings;
+using UnityEngine;
+
+public class ActionKeyBindings : KeyboardBindings
+{
+
+    public KeyBind AttackKey { get; set; } = new KeyBind("Attack", KeyCode.Mouse0);
+    public KeyBind DropKey { get; set; } = new KeyBind("Drop item", KeyCode.Q);
+    public KeyBind UseKey { get; set; } = new KeyBind("Use", KeyCode.Mouse1);
+    public KeyBind CrouchingKey {get; set;} = new KeyBind("Crouching", KeyCode.LeftShift);
+
+    public override KeyBind[] GetBinds()
+    {
+        return new KeyBind[]{AttackKey, DropKey, UseKey, CrouchingKey};
+    }
+}
