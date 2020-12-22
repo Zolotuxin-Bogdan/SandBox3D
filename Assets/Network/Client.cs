a1d44c2cadb33ba6fe40e0cce9f157c507afe3ce
Yeremeev <tonaros.kill@gmail.com>
2020-12-22 20:41:55 +0200

    added client and TcpClient without implementation

---
+++ Assets/Network/Client.cs
@@ -0,0 +1,5 @@
+﻿public static class Client
+{
+    public static string version { get; private set; } = "1.0.0";
+    public static uint protocolVersion { get; private set; } = 0;
+}
---
+++ Assets/Network/TcpClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.WebSockets;
+using System.Net.Sockets;
+using System.Collections.Generic;
+public class TcpClient 
+{
+    public static void Connect(string ipAddress)
+    {
+
+    }
+    public static void Connect(string ipAddress, ushort port)
+    {
+
+    }
+    public static ServerStatus GetStatus(string ipAddress)
+    {
+        return null;
+    }
+}
+
+public class ServerStatus
+{
+    public string version {get; private set;} = "";
+    public string description {get; private set;} = "";
+    public uint connected {get; private set;} = 0;
+    public uint maxConnections {get; private set;} = 0;
+    public string address {get; private set;} = "localhost";
+}
