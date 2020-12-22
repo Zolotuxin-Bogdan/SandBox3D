b0678cd33e135ef891d227d9bade2b18f6894296
Yeremeev <tonaros.kill@gmail.com>
2020-12-22 20:42:38 +0200

    added singleplayer menu without logic

--- Assets/Scripts/UI/SingleplayerMenuController.cs
+++ Assets/Scripts/UI/SingleplayerMenuController.cs
@@ -1,18 +1,77 @@
-﻿using System.Collections;
+﻿using TMPro;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.Events;
 
 public class SingleplayerMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button createNewWorld;
+    public Button playSelectedWorld;
+    public Button cancel;
+    public Button reCreate;
+    public Button rename;
+    public Button delete;
+    public ScrollRect worlds;
+    public InputField searchField;
+    
+    int selectedWorld = -1;
     void Start()
     {
-        
+        createNewWorld.onClick.AddListener(CreateWorldCallback);
+        playSelectedWorld.onClick.AddListener(RunWorldCallback);
+        cancel.onClick.AddListener(CancelCallback);
+        reCreate.onClick.AddListener(ReCreateCallback);
+        rename.onClick.AddListener(RenameCallback);
+        delete.onClick.AddListener(DeleteCallback);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DeleteCallback()
     {
-        
+        if (selectedWorld > -1)
+            Destroy(worlds.content.GetChild(selectedWorld));
+    }
+
+    private void RenameCallback()
+    {
+        // var text = RenameWorld(worlds.content.GetChild(selectedWorld));
+        throw new NotImplementedException();
+    }
+
+    private void ReCreateCallback()
+    {
+        // WorldLoader.Remove(worldname);
+        // WorldGenerator.Generate();
+        // action.Invoke(StartGame);
+        throw new NotImplementedException();
+    }
+
+    private void CancelCallback()
+    {
+        action.Invoke();
+        throw new NotImplementedException();
+    }
+
+    private void RunWorldCallback()
+    {
+        // WorldLoader.Load(worldname);
+        // action.Invoke(StartGame);
+        throw new NotImplementedException();
+    }
+
+    private void CreateWorldCallback()
+    {
+        // WorldGenerator.Generate();
+        // action.Invoke(StartGame);
+        throw new NotImplementedException();
+    }
+
+    protected UnityAction action;
+
+    public void AddListener(UnityAction action)
+    {
+        this.action = action;
     }
 }
