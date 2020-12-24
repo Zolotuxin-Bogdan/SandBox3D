914bd89437c1b046600af32f4e37dd577bd2f2fc
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-12-24 18:14:46 +0200

    Implemented First person camera mechanism

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,25 +7,33 @@ public class PlayerController : MonoBehaviour
     public float JumpHeight = 20.0f;
     public float Gravity = -9.8f;
 
+    public GameObject FirstPersonCamPosition;
+    public Camera FirstPersonCam;
+
     private float _verticalSpeed = 0;
 
     private readonly InputSystem _inputSystem = new InputSystem();
     private CharacterController _characterController;
+    private Camera _firstPersonCamera;
     private UnityAction<string> _action;
 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _firstPersonCamera = FirstPersonCam.GetComponent<Camera>();
     }
 
     void Update()
     {
         Move();
+        Dig();
     }
 
     void Move()
     {
-        var deltaX = _inputSystem.GetHorizontalMovementValue() * Speed ;
+        FirstPersonCam.transform.position = FirstPersonCamPosition.transform.position;
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, _firstPersonCamera.transform.rotation.eulerAngles.y, transform.rotation.z));
+        var deltaX = _inputSystem.GetHorizontalMovementValue() * Speed;
         var deltaZ = _inputSystem.GetVerticalMovementValue() * Speed;
         var movement = new Vector3(deltaX, 0, deltaZ);
         movement = Vector3.ClampMagnitude(movement, Speed);
@@ -41,6 +49,14 @@ public class PlayerController : MonoBehaviour
         _verticalSpeed += Gravity * Time.deltaTime;
         movement.y = _verticalSpeed;
         _characterController.Move(movement * Time.deltaTime);
+    }
+
+    void Dig()
+    {
+        Ray ray = FirstPersonCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 3))
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
     }
 
     public void OnDropItemTouched(UnityAction<string> action)
