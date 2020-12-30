b56897f60b18ea2c75617254050970170f4d5b30
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-12-30 14:50:02 +0200

    Added xml project config and its ConfigLoader script

---
+++ Assets/Scripts/ConfigLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml;
+using UnityEngine;
+
+public static class ConfigLoader
+{
+    public static XmlDocument GetConfig()
+    {
+        TextAsset textAsset = (TextAsset)Resources.Load("App");
+        XmlDocument xmldoc = new XmlDocument();
+        xmldoc.LoadXml(textAsset.text);
+        return xmldoc;
+    }
+}
