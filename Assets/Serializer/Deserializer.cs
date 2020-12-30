b31fa926d7dedae448841fe692d92f69e388e4dc
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2020-12-30 14:46:00 +0200

    Added JSON <-> Binnary serialization

--- Assets/Serializer/Deserializer.cs
+++ Assets/Serializer/Deserializer.cs
@@ -1,7 +1,11 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text;
+using Newtonsoft.Json;
 
 public static class Deserializer
 {
     public static T DeserializeJson<T>(string obj) =>
         JsonConvert.DeserializeObject<T>(obj);
+
+    public static string DeserializeBytes(byte[] obj) =>
+        Encoding.UTF8.GetString(obj);
 }
--- Assets/Serializer/Serializer.cs
+++ Assets/Serializer/Serializer.cs
@@ -1,8 +1,11 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text;
+using Newtonsoft.Json;
 
 public static class Serializer
 {
     public static string SerializeToJson<T>(T obj) =>
         JsonConvert.SerializeObject(obj);
 
+    public static byte[] SerializeToBytes(string obj) =>
+        Encoding.UTF8.GetBytes(obj);
 }
