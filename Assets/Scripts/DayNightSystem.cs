7a0dc9b113970a4d6642de399b9e1c1c0ae39aa5
Yeremeev <tonaros.kill@gmail.com>
2021-01-06 18:22:39 +0200

    Update Day Night System logic

---
+++ Assets/Scripts/DayNightSystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightSystem : MonoBehaviour
+{
+    [SerializeField] Light sun;
+    [SerializeField] Light moon;
+    [SerializeField] float secondsInFullDay = 120f;
+    
+    [Range(0, 1)] 
+    [SerializeField] float currentTime = 0;
+
+    [SerializeField] float timeMiltiplier = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateLight();
+
+        currentTime += (Time.deltaTime / secondsInFullDay) * timeMiltiplier;
+
+        if (currentTime >= 1)
+        {
+            currentTime = 0;
+        }
+    }
+
+    private void RotateLight()
+    {
+        sun.transform.localRotation = Quaternion.Euler((currentTime * 360f) - 90, 170, 0);
+        moon.transform.localRotation = Quaternion.Euler((currentTime * 360f) + 90, 170, 0);
+    }
+}
