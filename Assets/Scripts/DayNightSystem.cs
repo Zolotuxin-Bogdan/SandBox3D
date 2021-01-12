b8c6863cd508b3d1f2db4b5f47e50c6effa23dc2
Yeremeev <tonaros.kill@gmail.com>
2021-01-12 16:25:42 +0200

    finished skybox shader

--- Assets/Scripts/DayNightSystem.cs
+++ Assets/Scripts/DayNightSystem.cs
@@ -7,7 +7,13 @@ public class DayNightSystem : MonoBehaviour
 {
     [SerializeField] Light sun;
     [SerializeField] Light moon;
+    
+    [Header("Day/Night cycle Controls")]
+
     [SerializeField] float secondsInFullDay = 120f;
+    
+
+    [SerializeField] float celestialBodiesPos = 90;
     
     [Range(0, 1)] 
     [SerializeField] float currentTime = 0;
@@ -29,7 +35,14 @@ public class DayNightSystem : MonoBehaviour
 
     private void RotateLight()
     {
-        sun.transform.localRotation = Quaternion.Euler((currentTime * 360f) - 90, 170, 0);
-        moon.transform.localRotation = Quaternion.Euler((currentTime * 360f) + 90, 170, 0);
+        var rotationX = (currentTime * 360f) - celestialBodiesPos;
+        var rotationX1 = (currentTime * 360f) + celestialBodiesPos;
+        print(rotationX);
+        print(rotationX1);
+        sun.transform.localRotation = Quaternion.Euler(rotationX, 170, 0);
+        if ((180 - rotationX) < 0)
+            moon.transform.localRotation = Quaternion.Euler(360 + ((180 - rotationX) * -1), 170, 0);
+        else
+            moon.transform.localRotation = Quaternion.Euler(360 - ((180 + rotationX) * -1), 170, 0);
     }
 }
--- Assets/Scripts/SceneEditor.cs
+++ Assets/Scripts/SceneEditor.cs
@@ -1,11 +1,11 @@
 ﻿using Assets.InventorySystem;
-using Assets.InventorySystem.Items;
 using Assets.Scripts.DTO;
 using Assets.StorageSystem.StorageProviders;
 using UnityEngine;
 
 namespace Assets.Scripts
 {
+    [System.Obsolete("SceneEditor is deprecated.")]
     public class SceneEditor
     {
 
@@ -15,50 +15,51 @@ namespace Assets.Scripts
             meshLoader = new MeshProvider();
         }
 
-        public void AddItem(BaseItem item)
-        {
-            if (item == null)
-                throw new System.ArgumentNullException(nameof(item));
+        // public void AddItem(BaseItem item)
+        // {
+        //     if (item == null)
+        //         throw new System.ArgumentNullException(nameof(item));
 
-            var _item = new GameObject(item.name);
-            _item.AddComponent<Rigidbody>();
-            _item.AddComponent<MeshRenderer>();
-            //_item.AddComponent<Item>().item = item;
-            _item.AddComponent<MeshFilter>().mesh = meshLoader.ImportMeshBySlug(item.slug) ?? 
-                                                    throw new System.Exception(
-                                                        $"MeshImportingError: Mesh by {nameof(item.slug)} not found!"
-                                                    );
-        }
+        //     var _item = new GameObject(item.name);
+        //     _item.AddComponent<Rigidbody>();
+        //     _item.AddComponent<MeshRenderer>();
+        //     //_item.AddComponent<Item>().item = item;
+        //     _item.AddComponent<MeshFilter>().mesh = meshLoader.ImportMeshBySlug(item.slug) ?? 
+        //                                             throw new System.Exception(
+        //                                                 $"MeshImportingError: Mesh by {nameof(item.slug)} not found!"
+        //                                             );
+        // }
 
         public bool AddItem(BlockDto blockDto) {
-            try
-            {
-                var resources = ResourcePackStorageProvider.Instance.LoadResourcePack();
-                var blockInfo = resources.Blocks[blockDto.BlockId];
-                var spawnedBlock = Object.Instantiate(BlockTypeManager.Instance.GetBlockTypeByName($"{blockInfo.BlockTypeName}"), blockDto.Position, Quaternion.Euler(new Vector3(-90, 0, 0)));
-                spawnedBlock.name = blockInfo.BlockName;
-                BlockMaterialManager.Instance.GetBlockMaterialByName(blockInfo.BlockMaterialType.ToString());
-                var blockTexture = new Texture2D(16, 16, TextureFormat.RGBA32, false);
-                blockTexture.LoadImage(GetTextureBytes(blockInfo.BlockTexturePath));
-                blockTexture.filterMode = FilterMode.Point;
-                spawnedBlock.GetComponent<Renderer>().material.SetTexture("_MainTex", blockTexture);
+            throw new System.NotImplementedException();
+            // try
+            // {
+            //     var resources = ResourcePackStorageProvider.Instance.LoadResourcePack();
+            //     var blockInfo = resources.Blocks[blockDto.BlockId];
+            //     var spawnedBlock = Object.Instantiate(BlockTypeManager.Instance.GetBlockTypeByName($"{blockInfo.BlockTypeName}"), blockDto.Position, Quaternion.Euler(new Vector3(-90, 0, 0)));
+            //     spawnedBlock.name = blockInfo.BlockName;
+            //     BlockMaterialManager.Instance.GetBlockMaterialByName(blockInfo.BlockMaterialType.ToString());
+            //     var blockTexture = new Texture2D(16, 16, TextureFormat.RGBA32, false);
+            //     blockTexture.LoadImage(GetTextureBytes(blockInfo.BlockTexturePath));
+            //     blockTexture.filterMode = FilterMode.Point;
+            //     spawnedBlock.GetComponent<Renderer>().material.SetTexture("_MainTex", blockTexture);
 
-                var item = new Assets.InventorySystem.Items.Block{
-                    count = 1, 
-                    isContainer = false, 
-                    descriptiom = $"{blockInfo.BlockProperties}", 
-                    name = blockInfo.BlockName, 
-                    slug = $"item.block.{blockInfo.BlockName}_{blockInfo.BlockTypeName}:{blockInfo.BlockId}"
-                }; 
-                spawnedBlock.AddComponent<Item>().item = (BaseItem)item;
+            //     var item = new Assets.InventorySystem.Items.Block{
+            //         count = 1, 
+            //         isContainer = false, 
+            //         descriptiom = $"{blockInfo.BlockProperties}", 
+            //         name = blockInfo.BlockName, 
+            //         slug = $"item.block.{blockInfo.BlockName}_{blockInfo.BlockTypeName}:{blockInfo.BlockId}"
+            //     }; 
+            //     spawnedBlock.AddComponent<Item>().item = (BaseItem)item;
             
-                return true;    
-            }
-            catch (System.Exception e)
-            {
-                Debug.LogError(e);
-                return false;
-            }
+            //     return true;    
+            // }
+            // catch (System.Exception e)
+            // {
+            //     Debug.LogError(e);
+            //     return false;
+            // }
        
         }
 
