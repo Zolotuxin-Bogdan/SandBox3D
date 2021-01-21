0657677887e2d51479497a27d45e6d028d1606c1
Zolotuxin-Bogdan <b.zolotuxin@gmail.com>
2021-01-21 14:09:34 +0200

    Code refactoring in PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ namespace Assets.Scripts
 {
     public class PlayerController : MonoBehaviour
     {
+        private const double FIFTY_DEGREE = 50.001;
+
         public float Speed = 6.0f;
         public float JumpHeight = 20.0f;
         public float Gravity = -9.8f;
@@ -62,12 +64,24 @@ namespace Assets.Scripts
             FirstPersonCam.transform.position = FirstPersonCamPosition.transform.position;
             var mouseDirection = _inputSystem.GetMouseXAxis();
             var camRotationY = FirstPersonCam.transform.rotation.eulerAngles.y;
+
+            //
+            // Rounding player model rotation to avoid non-integer values
+            //
             transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, (float)Math.Round(transform.rotation.eulerAngles.y), transform.rotation.z));
+            
+            //
+            // Rotate player model body by camera when rotation delta between them equal 50 degrees
+            //
+
+            //
+            // When Right rotation
+            //
             if (mouseDirection > 0)
             {
                 if (transform.rotation.eulerAngles.y >= 310)
                 {
-                    if (camRotationY <= 50 && 360 + camRotationY >= transform.rotation.eulerAngles.y + 50f)
+                    if (camRotationY <= FIFTY_DEGREE && 360 + camRotationY >= transform.rotation.eulerAngles.y + FIFTY_DEGREE)
                     {
                         if (transform.rotation.eulerAngles.y + 10 > 360)
                         {
@@ -80,17 +94,21 @@ namespace Assets.Scripts
                         }
                     }
                 }
-                else if (camRotationY >= transform.rotation.eulerAngles.y + 50f)
+                else if (camRotationY >= transform.rotation.eulerAngles.y + FIFTY_DEGREE)
                 {
                     transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.eulerAngles.y + 10, transform.rotation.z));
 
                 }
             }
+            
+            //
+            // When Left rotation
+            //
             else if (mouseDirection < 0)
             {
-                if (transform.rotation.eulerAngles.y < 51)
+                if (transform.rotation.eulerAngles.y <= FIFTY_DEGREE)
                 {
-                    if (camRotationY >= 300 && camRotationY - 360 < transform.rotation.eulerAngles.y - 51f)
+                    if (camRotationY >= 300 && camRotationY - 360 < transform.rotation.eulerAngles.y - FIFTY_DEGREE)
                     {
                         if (transform.rotation.eulerAngles.y - 10 < 0)
                         {
@@ -105,16 +123,39 @@ namespace Assets.Scripts
                         }
                     }
                 }
-                else if (camRotationY <= transform.rotation.eulerAngles.y - 51f)
+                else if (camRotationY <= transform.rotation.eulerAngles.y - FIFTY_DEGREE)
                 {
                     transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x,
                         transform.rotation.eulerAngles.y - 10, transform.rotation.z));
 
                 }
             }
+            
+            //
+            // Follow player model head by camera direction
+            //
             PlayerHead.transform.rotation = Quaternion.Euler(new Vector3(_firstPersonCamera.transform.rotation.eulerAngles.x * -1, _firstPersonCamera.transform.rotation.eulerAngles.y + 180f, transform.rotation.z));
+            
+            //
+            // User move input
+            //
             var deltaX = _inputSystem.GetHorizontalMovementValue() * Speed;
             var deltaZ = _inputSystem.GetVerticalMovementValue() * Speed;
+            
+            //
+            // Follow player model by camera direction
+            //
+            if (deltaZ > 0)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation,
+                    Quaternion.Euler(transform.rotation.eulerAngles.x, camRotationY, transform.rotation.eulerAngles.z), 
+                    200 * Time.deltaTime);
+                
+            }
+            
+            //
+            // Player movement
+            //
             var movement = new Vector3(deltaX, 0, deltaZ);
             movement = Vector3.ClampMagnitude(movement, Speed);
             movement = transform.TransformDirection(movement);
