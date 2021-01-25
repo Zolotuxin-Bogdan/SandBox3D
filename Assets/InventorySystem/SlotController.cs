ebe0d57d9b270b534c195938e15f5b7f974ad446
Yeremeev <tonaros.kill@gmail.com>
2021-01-25 16:27:49 +0200

    added tooltip, amount and durability for slots

--- Assets/InventorySystem/SlotController.cs
+++ Assets/InventorySystem/SlotController.cs
@@ -1,41 +1,105 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using Assets.InventorySystem.Items;
 using TMPro;
+using System;
 
 namespace Assets.InventorySystem.Controllers
 {
-    public class SlotController : MonoBehaviour {
+    public class SlotController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
         
         public Image icon;
 
         public UIItem item;
 
+        [SerializeField] GameObject tooltip;
+        [SerializeField] GameObject textMeshPro;
+        [SerializeField] GameObject progressBar;
+        Rect rect;
+        private void Awake() {
+            rect = GetComponent<RectTransform>().rect;
+        }
+
         public void AddItem(UIItem item) {
-            // if (item.name.Equals(this.item?.name)) {
-            //     this.item.amount += item.amount;
-            // }
-            // else 
-            //     this.item = item;
+            if (item.durability > 0)
+                ShowDurability(item.durability);
+
+            if (item.amount > 1)
+                ShowAmount(item.amount);
+
             this.item = item;
-            icon.color = Color.red;
-            //icon.sprite = item.icon;
+            icon.sprite = item.icon;
             icon.enabled = true;
+        }
+
+        private void ShowAmount(int amount)
+        {
+            textMeshPro.GetComponent<TextMeshProUGUI>().text = amount.ToString();
+            textMeshPro.SetActive(true);
+        }
+
+        private void ShowDurability(int durability)
+        {
+            progressBar.GetComponent<Slider>().value = durability;
+            progressBar.SetActive(true);
         }
 
         public void ClearSlot()
         {
             item = null;
-            // icon.sprite = null;
-            // icon.enabled = false;
+            icon.sprite = null;
+            icon.enabled = false;
         }
 
-        public void SetItemAmount(int amount = 1) {
-            throw new System.NotImplementedException();
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (item != null) {
+                if (item.name != null && item.name != "") {
+                    if (item.description != null && item.description != "") {
+                        tooltip.GetComponent<RectTransform>().position = 
+                            new Vector2(
+                                Input.mousePosition.x + 70, 
+                                Input.mousePosition.y + 10
+                            );
+                        
+                        var labels = tooltip.GetComponentsInChildren<TextMeshProUGUI>();
+                        labels[0].text = item.name;
+                        labels[1].text = item.description;
+                        
+                        if (item.name.Length > item.description.Length) {
+                            var size = tooltip.GetComponent<RectTransform>().sizeDelta;
+                            size = new Vector2(size.x, item.name.Length * 8);
+                            tooltip.GetComponent<RectTransform>().sizeDelta = size;
+                        } else {
+                            var size = tooltip.GetComponent<RectTransform>().sizeDelta;
+                            size = new Vector2(size.x, item.description.Length * 8);
+                            tooltip.GetComponent<RectTransform>().sizeDelta = size;
+                        }
+                    
+                    } else {
+                        tooltip.GetComponent<RectTransform>().position = 
+                            new Vector2(
+                                Input.mousePosition.x + 50, 
+                                Input.mousePosition.y + 10
+                            );
+                    
+                        var labels = tooltip.GetComponentsInChildren<TextMeshProUGUI>();
+                        labels[0].text = item.name;
+                        labels[1].text = null;
+                    
+                        var size = tooltip.GetComponent<RectTransform>().sizeDelta;
+                        size = new Vector2(item.name.Length * 7.6f, 20);
+                        tooltip.GetComponent<RectTransform>().sizeDelta = size;
+                    }
+                    tooltip.SetActive(true);
+                }
+            }
         }
 
-        public void SetToolTip(string data) {
-            GUI.Label(new Rect(), data, GUI.tooltip);
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            tooltip.SetActive(false);
         }
-    }
+   }
 }
