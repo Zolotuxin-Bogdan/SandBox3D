7751e058cf041c9023f23156985ec1a31b54b4bf
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2021-01-26 12:23:57 +0200

    Implemented Idle state

--- Assets/Scripts/States/IdleState.cs
+++ Assets/Scripts/States/IdleState.cs
@@ -1,17 +1,24 @@
 ﻿using Assets.FSM;
+using UnityEngine;
 
 namespace Assets.Scripts.States
 {
     public class IdleState: State
     {
+        private Animator _animator;
+
+        public IdleState()
+        {
+            _animator = StatesMachineManager.instance.Player.GetComponent<Animator>();
+        }
         public override void Start()
         {
-            throw new System.NotImplementedException();
+            _animator.Play(StatesMachineManager.instance.IDLE_ANIMATION);
         }
 
         public override void Close()
         {
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException();
         }
     }
 }
