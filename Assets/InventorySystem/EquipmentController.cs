5a02feecf920541a955e69e7caadad5f6a606530
Yeremeev <tonaros.kill@gmail.com>
2021-01-27 17:28:15 +0200

    added equipment controller without implementation

---
+++ Assets/InventorySystem/EquipmentController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.InventorySystem.Controllers;
+using UnityEngine;
+
+namespace Assets.InventorySystem
+{
+    public class EquipmentController : MonoBehaviour
+    {
+        [SerializeField] GameObject slot;
+        [SerializeField] SlotController[] slots;
+        [SerializeField] readonly int SLOTS_COUNT;
+
+        protected void Awake() {
+            
+        }
+
+        protected void BuildContent() {
+            for (int i = 0; i < SLOTS_COUNT; i++)
+            {
+                Instantiate(slot, transform).SetActive(true);
+            }
+            slots = GetComponentsInChildren<SlotController>();
+        }
+
+        protected void UpdateUI() {
+
+        }
+    }
+}
+
