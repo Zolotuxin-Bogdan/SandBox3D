04969aa160f83b95e06c8318140e7b952c7dc9b2
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2021-01-28 13:54:42 +0200

    Created PerlinNoiseGeneration class

---
+++ Assets/WorldGeneration/Implementations/PerlinNoiseGeneration.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.DTO;
+using Assets.WorldGeneration.Interfaces;
+
+namespace Assets.WorldGeneration.Implementations
+{
+    public class PerlinNoiseGeneration : IGenerator
+    {
+        public List<BlockDto> GetBlocksDto()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
