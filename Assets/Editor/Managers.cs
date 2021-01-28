6cc866e6ccb6f73ffc4d272c9e15aa8f41ddb7cd
Yeremeev <tonaros.kill@gmail.com>
2021-01-28 13:38:43 +0200

    update ToolsManager functionality

--- Assets/Editor/Managers.cs
+++ Assets/Editor/Managers.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 using UnityEditor;
 using Assets.StorageSystem.StorageProviders;
@@ -5,8 +6,10 @@ using Assets.Scripts;
 
 namespace Assets.Editor
 {
-    public class Managers: ScriptableWizard {
-
+    public class Managers: ScriptableWizard
+    {
+        [SerializeField] string objectName = "ToolsObject";
+        [Header("\n")]
         [Tooltip("Resource Pack Storage Provider")]
         [SerializeField] bool rPStorageProvider;
 
@@ -28,29 +31,74 @@ namespace Assets.Editor
 
         [MenuItem("SandBox3D/ToolsManager")]
         private static void MenuEntryCall() {
-            DisplayWizard<Managers>("Select Managers");
+            DisplayWizard<Managers>("Select Managers", "Create", "Update");
         }
 
         private void OnWizardCreate() {
-            GameObject go = new GameObject("ToolsObject");
+            GameObject go = new GameObject(objectName);
             if (rPStorageProvider)
                 go.AddComponent<ResourcePackStorageProvider>();
+
             if (rPManager)
                 go.AddComponent<ResourcePackManager>();
+
             if (bTManager)
                 go.AddComponent<BlockTypeManager>();
+
             if (bMManager)
                 go.AddComponent<BlockMaterialManager>();
+
             if (sceneEditor)
                 go.AddComponent<SceneEditor>();
+
             if (pSManager)
                 go.AddComponent<PlayerStatesManager>();
+
             if (inputSystem)
                 go.AddComponent<InputSystem>();
         }
 
         private void OnWizardUpdate() {
             helpString = "Create gameobject with selected tools";
+        }
+
+        private void OnWizardOtherButton()
+        {
+            var objects = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+            var gameObject = objects.FirstOrDefault(o => o.name == objectName);
+                if (gameObject != null)
+                {
+                    if (rPStorageProvider)
+                        if (gameObject.GetComponent<ResourcePackStorageProvider>() == null)
+                            gameObject.AddComponent<ResourcePackStorageProvider>();
+
+                    if (rPManager)
+                        if (gameObject.GetComponent<ResourcePackManager>() == null)
+                            gameObject.AddComponent<ResourcePackManager>();
+
+                    if (bTManager)
+                        if (gameObject.GetComponent<BlockTypeManager>() == null)
+                            gameObject.AddComponent<BlockTypeManager>();
+
+                    if (bMManager)
+                        if (gameObject.GetComponent<BlockMaterialManager>() == null)
+                            gameObject.AddComponent<BlockMaterialManager>();
+
+                    if (sceneEditor)
+                        if (gameObject.GetComponent<SceneEditor>() == null)
+                            gameObject.AddComponent<SceneEditor>();
+
+                    if (pSManager)
+                        if (gameObject.GetComponent<PlayerStatesManager>() == null)
+                            gameObject.AddComponent<PlayerStatesManager>();
+
+                    if (inputSystem)
+                        if (gameObject.GetComponent<InputSystem>() == null)
+                            gameObject.AddComponent<InputSystem>();
+                } else
+                {
+                    EditorUtility.DisplayDialog("Warning", "This GameObject not found on scene", "Close");
+                }
         }
     }
 }
