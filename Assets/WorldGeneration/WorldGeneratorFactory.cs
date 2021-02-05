930c9f8b3a1f97b754b2525808f6871164aa797e
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2021-02-05 17:19:34 +0200

    Implemented WorldGenerator factory

---
+++ Assets/WorldGeneration/WorldGeneratorFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Assets.Scripts.Enums;
+using Assets.WorldGeneration.Implementations;
+using Assets.WorldGeneration.Interfaces;
+
+namespace Assets.WorldGeneration
+{
+    public static class WorldGeneratorFactory
+    {
+        public static IWorldGenerator GetWorldGenerator(WorldGenerationType generationType)
+        {
+            switch (generationType)
+            {
+                case WorldGenerationType.Flat:
+                    return new FlatWorldTypeGeneration();
+                case WorldGenerationType.Perlin:
+                    return new PerlinNoiseGeneration();
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(generationType));
+            }
+        }
+    }
+}
