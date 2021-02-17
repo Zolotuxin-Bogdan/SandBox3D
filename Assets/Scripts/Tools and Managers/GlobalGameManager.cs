42716cd5f3238607e69ab280f4b52c91b1b6edef
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2021-02-17 15:52:42 +0200

    Created and Implemented GlobalGameManager

---
+++ Assets/Scripts/Tools and Managers/GlobalGameManager.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts.Tools_and_Managers;
+using Assets.StorageSystem.StorageProviders;
+using UnityEngine;
+
+public class GlobalGameManager : MonoBehaviour
+{
+    public static GlobalGameManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance == this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        ResourcePackManager.Instance.UnpackTextures();
+        ResourcePackStorageProvider.Instance.CreateResourcePack();
+    }
+}
