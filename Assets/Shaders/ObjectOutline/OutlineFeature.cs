fe42646e6909149bd1505260db0564a89478f27a
Zolotuxin-Bogdan <b.zolotuxin@gmail.com>
2021-02-18 17:48:21 +0200

    Added shader outline

---
+++ Assets/Shaders/ObjectOutline/OutlineFeature.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Assets.Shaders.ObjectOutline
+{
+    public class OutlineFeature : ScriptableRendererFeature
+    {
+        [SerializeField] private string _renderTextureName;
+        [SerializeField] private RenderSettings _renderSettings;
+
+        private RenderTargetHandle _renderTexture;
+        private CustomRenderObjectPass _renderPass;
+
+        [Serializable]
+        public class RenderSettings
+        {
+            public Material OverrideMaterial = null;
+            public LayerMask LayerMask = 0;
+        }
+
+        [SerializeField] private string _bluredTextureName;
+        [SerializeField] private BlurSettings _blurSettings;
+        private RenderTargetHandle _bluredTexture;
+        private BlurRenderPass _blurPass;
+
+        [Serializable]
+        public class BlurSettings
+        {
+            public Material BlurMaterial;
+            public int DownSample = 1;
+            public int PassesCount = 1;
+        }
+
+        [SerializeField] private Material _outlineMaterial;
+        private OutlineRenderPass _outlinePass;
+
+        [SerializeField] private RenderPassEvent _renderPassEvent;
+
+
+        public override void Create()
+        {
+            _renderTexture.Init(_renderTextureName);
+
+            _renderPass = new CustomRenderObjectPass(_renderTexture, _renderSettings.LayerMask, _renderSettings.OverrideMaterial);
+
+            _bluredTexture.Init(_bluredTextureName);
+
+            _blurPass = new BlurRenderPass(_blurSettings.BlurMaterial, _blurSettings.DownSample, _blurSettings.PassesCount);
+
+            _outlinePass = new OutlineRenderPass(_outlineMaterial);
+
+            _renderPass.renderPassEvent = _renderPassEvent;
+            _blurPass.renderPassEvent = _renderPassEvent;
+            _outlinePass.renderPassEvent = _renderPassEvent;
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            renderer.EnqueuePass(_renderPass);
+            renderer.EnqueuePass(_blurPass);
+            renderer.EnqueuePass(_outlinePass);
+        }
+    }
+}
---
+++ Assets/Shaders/ObjectOutline/OutlineRenderPass.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Assets.Shaders.ObjectOutline
+{
+    public class OutlineRenderPass : ScriptableRenderPass
+    {
+        private string _profilerTag = "Outline";
+        private Material _material;
+
+        public OutlineRenderPass(Material material)
+        {
+            _material = material;
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            var cmd = CommandBufferPool.Get(_profilerTag);
+
+            using (new ProfilingSample(cmd, _profilerTag))
+            {
+                var mesh = RenderingUtils.fullscreenMesh;
+                cmd.DrawMesh(mesh, Matrix4x4.identity, _material, 0, 0);
+            }
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+    }
+}
