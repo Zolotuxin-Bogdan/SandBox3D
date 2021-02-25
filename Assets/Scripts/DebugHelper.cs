b9be2b4a135c85c176aff09d6518ce47f664300d
Yeremeev <tonaros.kill@gmail.com>
2021-02-25 16:10:02 +0200

    remove deprecated blue noise call logic

--- Assets/Scripts/DebugHelper.cs
+++ Assets/Scripts/DebugHelper.cs
@@ -19,45 +19,28 @@ public class DebugHelper : MonoBehaviour
         renderer.material.SetTexture("_BaseMap", perlinTexture);
         inst = BlockInstanceManager.Instance;
         _blueNoiseTexture = perlinTexture;
-        Test_BlueNoiseHighMapGeneration(perlinTexture);
+        GenerateTreesMap(perlinTexture.width, perlinTexture.height);
     }
 
     public void GenerateWorld()
     {
         inst.GenerateWorld(new PerlinNoiseGeneration());
     }
-
-    public void GenerateBNWorld()
-    {
-        inst.GenerateWorld(new BlueNoiseGeneration(_blueNoiseTexture.width, _blueNoiseTexture.height, _blueNoiseTexture));
-    }
-
+    
     public void Combine()
     {
         inst.Combine();
     }
 
-    public void Test_BlueNoiseHighMapGeneration(Texture2D noise)
+    public void GenerateTreesMap(int width, int height)
     {
-        var blueNoisePanel = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        blueNoisePanel.transform.position = new Vector3(perlinPanel.transform.position.x * 1.5f,
+        var treesMap = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        treesMap.transform.position = new Vector3(perlinPanel.transform.position.x * 1.5f,
             perlinPanel.transform.position.y, perlinPanel.transform.position.z);
-        var generator = new BlueNoiseGeneration(noise.width, noise.height, noise);
-        var blueNoiseTex = generator.GenerateBlueNoise();
-        var renderer = blueNoisePanel.GetComponent<Renderer>();
-        renderer.material.SetTexture("_BaseMap", blueNoiseTex);
-        Test_BlueNoiseGeneration(blueNoiseTex);
-    }
-
-    public void Test_BlueNoiseGeneration(Texture2D noise)
-    {
-        var blueNoisePanel = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        blueNoisePanel.transform.position = new Vector3(perlinPanel.transform.position.x * 2.2f,
-            perlinPanel.transform.position.y, perlinPanel.transform.position.z);
-        var generator = new BlueNoiseGeneration(noise.width, noise.height, noise);
-        var blueNoiseTex = generator.GenerateTexture();
-        var renderer = blueNoisePanel.GetComponent<Renderer>();
-        renderer.material.SetTexture("_BaseMap", blueNoiseTex);
+        var generator = new BlueNoiseGeneration(width, height);
+        var texture = generator.NoiseGen();
+        var renderer = treesMap.GetComponent<Renderer>();
+        renderer.material.SetTexture("_BaseMap", texture);
     }
 
     public void GenerateTree()
