e6b991da41d1392d80fc0d0b3918fa23d2f3cc2a
Bogdan Zolotukhin <b.zolotuxin@gmail.com>
2021-03-03 18:23:58 +0200

    Added xml configs for different development stages

--- Assets/Scripts/ConfigLoader.cs
+++ Assets/Scripts/ConfigLoader.cs
@@ -7,7 +7,11 @@ namespace Assets.Scripts
     {
         public static XmlDocument GetConfig()
         {
-            TextAsset textAsset = (TextAsset)Resources.Load("App");
+#if UNITY_EDITOR
+            TextAsset textAsset = (TextAsset)Resources.Load("App_Debug");
+#else
+            TextAsset textAsset = (TextAsset)Resources.Load("App_Release");
+#endif
             XmlDocument xmldoc = new XmlDocument();
             xmldoc.LoadXml(textAsset.text);
             return xmldoc;
