070c6f0a90e80fafdaf53bd49ff6e78b37678022
Yeremeev <tonaros.kill@gmail.com>
2021-03-03 12:08:28 +0200

    added Localizer class that supported localization interface

---
+++ Assets/LocalizationSystem/Localizer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.LocalizationSystem
+{
+    public class Localizer
+    {
+        public static void RegisterController<TController>(TController controller) where TController : ILocalization
+        {
+            controller.SetLocalization();
+        }
+    }
+}
