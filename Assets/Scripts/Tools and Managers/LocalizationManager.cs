5d5429b48891d5754fd9696ccb62111bc47e5d06
Yeremeev <tonaros.kill@gmail.com>
2021-03-03 12:09:07 +0200

    implemented localization manager

---
+++ Assets/Scripts/Tools and Managers/LocalizationManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Assets.LocalizationSystem;
+using Assets.Scripts.Enums;
+using Assets.Scripts.UI;
+using UnityEngine;
+
+namespace Assets.Scripts.Tools_and_Managers
+{
+    public class LocalizationManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            UpdateLocalization();
+        }
+
+        public static void UpdateLocalization()
+        {
+            Localizer.RegisterController(MenuController.Instance);
+        }
+    }
+}
